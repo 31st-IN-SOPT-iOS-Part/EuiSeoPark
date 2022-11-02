001407e73718c0e3ac77300cf05b2df4049935b9
kpk0616 <kpk0616@naver.com>
2022-11-02 22:13:38 +0900

    [FEAT] Feat TableView in PhotoGridVC

--- IN_SOPT_Third_Task/IN_SOPT_Third_Task/Sources/ViewControllers/PhotoGridViewController.swift
+++ IN_SOPT_Third_Task/IN_SOPT_Third_Task/Sources/ViewControllers/PhotoGridViewController.swift
@@ -32,16 +32,39 @@ class PhotoGridViewController: UIViewController {
         $0.addTarget(self, action: #selector(touchUpSettingButton), for: .touchUpInside)
     }
     
+    private lazy var kakaoProfileTableView = UITableView.init(frame: .zero, style: .grouped).then {
+        $0.backgroundColor = .clear
+//        $0.tableHeaderView = myProfileView
+        $0.separatorStyle = .none
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.delegate = self
+        $0.dataSource = self
+    }
+    
+    var kakaoProfileList: [KakaoProfileModel] = [
+        KakaoProfileModel(profileImage: "profileImage1", name: "솝트", statusMessage: "사랑해"),
+        KakaoProfileModel(profileImage: "profileImage2", name: "솝트", statusMessage: "사랑해"),
+        KakaoProfileModel(profileImage: "profileImage3", name: "솝트", statusMessage: "사랑해"),
+        KakaoProfileModel(profileImage: "profileImage4", name: "솝트", statusMessage: "사랑해"),
+        KakaoProfileModel(profileImage: "profileImage5", name: "솝트", statusMessage: "사랑해"),
+        KakaoProfileModel(profileImage: "profileImage6", name: "솝트", statusMessage: "사랑해"),
+        KakaoProfileModel(profileImage: "profileImage7", name: "솝트", statusMessage: "사랑해"),
+        KakaoProfileModel(profileImage: "profileImage8", name: "솝트", statusMessage: "사랑해"),
+        KakaoProfileModel(profileImage: "profileImage9", name: "솝트", statusMessage: "사랑해")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setLayout()
+        register()
+        kakaoProfileTableView.layoutIfNeeded()
     }
 }
 
 extension PhotoGridViewController {
     private func setLayout() {
         view.backgroundColor = .white
-        view.addSubviews(chatTopView)
+        view.addSubviews(chatTopView, kakaoProfileTableView)
         chatTopView.snp.makeConstraints {
             $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(52)
@@ -66,6 +89,13 @@ extension PhotoGridViewController {
             $0.trailing.equalTo(settingIconButton.snp.leading).offset(-12)
             $0.width.height.equalTo(19)
         }
+        
+        kakaoProfileTableView.snp.makeConstraints {
+            $0.top.equalTo(chatTopView.snp.bottom)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalToSuperview()
+            $0.height.equalTo(50 * kakaoProfileList.count)
+        }
     }
     
     @objc
@@ -83,5 +113,33 @@ extension PhotoGridViewController {
         let moreVC = MoreViewController()
         moreVC.modalPresentationStyle = .fullScreen
         self.present(moreVC, animated: true, completion: nil)
+    }
+    
+    private func register() {
+        kakaoProfileTableView.register(KakaoProfileTableViewCell.self, forCellReuseIdentifier: KakaoProfileTableViewCell.identifier)
+    }
+    
+}
+
+extension PhotoGridViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 73
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        50
+    }
+}
+
+extension PhotoGridViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kakaoProfileList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let profileViewCell = tableView.dequeueReusableCell(withIdentifier: KakaoProfileTableViewCell.identifier, for: indexPath) as? KakaoProfileTableViewCell else { return UITableViewCell() }
+        
+        profileViewCell.dataBind(model: kakaoProfileList[indexPath.row])
+        return profileViewCell
     }
 }
