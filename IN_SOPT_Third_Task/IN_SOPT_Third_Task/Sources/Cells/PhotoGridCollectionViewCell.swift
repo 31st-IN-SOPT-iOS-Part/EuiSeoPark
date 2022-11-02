8e555819a15c325221ed2b2d3b0580b53d314792
kpk0616 <kpk0616@naver.com>
2022-11-03 01:32:42 +0900

    [ADD] Add PhotoGridCollectionViewCell

---
+++ IN_SOPT_Third_Task/IN_SOPT_Third_Task/Sources/Cells/PhotoGridCollectionViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  PhotoGridCollectionViewCell.swift
+//  IN_SOPT_Third_Task
+//
+//  Created by 박의서 on 2022/11/03.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class PhotoGridCollectionViewCell: UICollectionViewCell {
+    // MARK: - identifire
+    static let identifire = "PhotoGridCollectionViewCell"
+    
+    // MARK: - UI Components
+    private let photoImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension PhotoGridCollectionViewCell {
+    private func setLayout() {
+        backgroundColor = .clear
+        contentView.backgroundColor = .clear
+        
+        contentView.addSubviews(photoImageView)
+        photoImageView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    func dataBind(model: PhotoGridModel) {
+        photoImageView.image = UIImage(named: model.image)
+    }
+}
--- IN_SOPT_Third_Task/IN_SOPT_Third_Task/Sources/ViewControllers/PhotoGridViewController.swift
+++ IN_SOPT_Third_Task/IN_SOPT_Third_Task/Sources/ViewControllers/PhotoGridViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class PhotoGridViewController: UIViewController {
     
-    private let moreImageTopView = UIView()
+    private let photoGridTopView = UIView()
 
     private lazy var closeIconButton = UIButton().then {
         $0.setImage(UIImage(named: "iconClose"), for: .normal)
@@ -21,7 +21,7 @@ class PhotoGridViewController: UIViewController {
         $0.text = "최근 항목"
     }
     
-    private let photosCollectionView: UICollectionView = {
+    private let photoGridCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         
@@ -33,7 +33,15 @@ class PhotoGridViewController: UIViewController {
         return collectionView
     }()
     
+    var photoGridList: [PhotoGridModel] = [
+        PhotoGridModel(image: "selecImage"), PhotoGridModel(image: "galleryImage1"), PhotoGridModel(image: "galleryImage2"), PhotoGridModel(image: "galleryImage3"), PhotoGridModel(image: "galleryImage4"), PhotoGridModel(image: "galleryImage5"), PhotoGridModel(image: "galleryImage6"), PhotoGridModel(image: "galleryImage7"), PhotoGridModel(image: "galleryImage8"), PhotoGridModel(image: "galleryImage9"), PhotoGridModel(image: "galleryImage10"), PhotoGridModel(image: "galleryImage11"), PhotoGridModel(image: "galleryImage12"), PhotoGridModel(image: "galleryImage13"), PhotoGridModel(image: "galleryImage14"), PhotoGridModel(image: "galleryImage15"), PhotoGridModel(image: "galleryImage16"), PhotoGridModel(image: "galleryImage17"), PhotoGridModel(image: "galleryImage18"), PhotoGridModel(image: "galleryImage19"), PhotoGridModel(image: "galleryImage20"), PhotoGridModel(image: "galleryImage21"), PhotoGridModel(image: "galleryImage22"), PhotoGridModel(image: "galleryImage23")
+    ]
     
+    // MARK: - constants
+    final let photoGridInset: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 4, right: 0)
+    final let photoGridLineSpacing: CGFloat = 7
+    final let photoGridInterItemSpacing: CGFloat = 9
+    final let photoGridCellHeight: CGFloat = 119
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,14 +52,14 @@ class PhotoGridViewController: UIViewController {
 extension PhotoGridViewController {
     private func setLayout() {
         view.backgroundColor = .white
-        view.addSubviews(moreImageTopView)
+        view.addSubviews(photoGridTopView, photoGridCollectionView)
         
-        moreImageTopView.snp.makeConstraints {
+        photoGridTopView.snp.makeConstraints {
             $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(38)
         }
         
-        moreImageTopView.addSubviews(closeIconButton, recentImageTextLabel)
+        photoGridTopView.addSubviews(closeIconButton, recentImageTextLabel)
         closeIconButton.snp.makeConstraints {
             $0.top.equalToSuperview().inset(7)
             $0.leading.equalToSuperview().inset(12)
@@ -61,6 +69,17 @@ extension PhotoGridViewController {
             $0.top.equalToSuperview().inset(11)
             $0.centerX.equalToSuperview()
         }
+        
+        photoGridCollectionView.snp.makeConstraints {
+            $0.top.equalTo(photoGridTopView.snp.bottom)
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func calculateCellHeight() -> CGFloat {
+            let count = CGFloat(photoGridList.count)
+            let heightCount = count / 3 + count.truncatingRemainder(dividingBy: 3)
+            return heightCount * photoGridCellHeight + (heightCount - 1) * photoGridLineSpacing + photoGridInset.top + photoGridInset.bottom
     }
     
     @objc
